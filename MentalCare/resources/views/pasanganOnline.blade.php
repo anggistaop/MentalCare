9d02499ac924056e9ad69aff61ea725502cb27cb
stefanieamadia <sekarhairani@gmail.com>
2023-11-27 20:19:35 +0700

    at Front End
    menambahkan tampilan ke laravel

---
+++ MentalCare/resources/views/pasanganOnline.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../konseling.css">
+    <title>Pasangan Online</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo">MentalCare</div>
+            <ul>
+                <li><a href="../../../../dashboard/index.html">Beranda</a></li>
+                    <li class="dropdown">
+                        <span>Konseling </span>
+                        <div class="dropdown-content">
+                            <a href="../../../konseling individu/konselingIndividu.html">Individu</a>
+                            <a href="../../konselingPasangan.html">Pasangan</a>
+                            <a href="../../../konseling keluarga/konselingKeluarga.html">Keluarga</a>
+                        </div>
+                        <li><a href="#">Psikolog</a></li>
+                        <li><a href="#">Artikel</a></li>
+                        <li><a href="#">Testimoni</a></li>
+                        <li><a href="#">Tentang Kami</a></li>
+            </ul>
+            <a class="account" href="account.html">
+                <img src="../../../../assets/img/account 1.png" class="account" alt="Gambar Akun Pengguna">
+            </a>
+        </nav>
+    </header>
+    <h1 class="judul">Hubungan dengan pasanganmu sedang tidak harmonis ?</h1>
+    <main>
+        <div class="isi-selengkapnya">
+                <div class="itemm-1">
+                    <img src="../../../../assets/img/pasangan - online.jpg" class="gambar"></img>
+                    <p>Selamat datang di MentalCare Pre Marriage atau Marriage - tempat di mana hubungan yang sehat dan bahagia menjadi fokus utama kami. Kami memahami bahwa setiap pasangan menghadapi tantangan unik dalam perjalanan hubungan mereka. MentalCare menawarkan layanan konsultasi pasangan yang ramah, mendalam, dan penuh perhatian. Bersama-sama, mari kita bangun hubungan yang sehat, penuh makna, dan penuh kebahagiaan.</p>
+                    <button class="selengkapnya">
+                        <a href="./layanan/pasanganOnline/index.html">Konsultasi Sekarang</a>
+                    </button>
+                </div>
+        </div>               
+    </main>
+</body>
+</html>
