3fa1406b41eff3ac0d820208430a86ce3266dc42
Rayhan Pangestu Wibowo <rayhanwibowo24@gmail.com>
2023-11-27 18:13:45 +0700

    Add Model and Database

--- MentalCare/app/Models/User.php
+++ MentalCare/app/Models/User.php
@@ -18,7 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
+        'gender',
+        'birthdate',
+        'username',
         'email',
         'password',
     ];
--- MentalCare/database/migrations/2014_10_12_000000_create_users_table.php
+++ MentalCare/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('gender');
+            $table->date('birthdate');
+            $table->string('username')->unique(); 
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
