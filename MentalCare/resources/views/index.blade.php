c5ad43125a0a5843196ffec5f2d26565683c837d
Fadhola Asandi <mardikha.555@gmail.com>
2023-12-18 14:16:55 +0700

    menambahkan meta tag sesuai ketentuan

--- MentalCare/resources/views/index.blade.php
+++ MentalCare/resources/views/index.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="dicoding:email" content="mardikha.555@gmail.com">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link
